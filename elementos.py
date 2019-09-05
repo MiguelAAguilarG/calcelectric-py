1d6f779536c03df5c9b14ca7d0251efb3b2f42c2
Miguel Angel Aguilar Garcia <migue_ag18@hotmail.com>
2019-09-04 21:35:35 -0500

    Aqui se agregaran todos los elementos electricos, como cargas

---
+++ elementos.py
@@ -0,0 +1,28 @@
+from Calculos import *
+
+class Carga(Calculos):
+    def __init__(self datos_entrada):
+        self.datos_entrada = datos_entrada
+
+        self.Sistema = self.datos_entrada['Sistema']
+        self.Voltaje = self.datos_entrada['Voltaje']
+        self.Carga = self.datos_entrada['Carga']
+        self.factor_carga = self.datos_entrada['factor_carga']
+        self.fp =self.datos_entrada['fp']
+        self.caida_tension = self.datos_entrada['caida_tension']
+        self.Longitud = self.datos_entrada['Longitud']
+        self.Tambiente = self.datos_entrada['Tambiente']
+        self.aislante_conductor = self.datos_entrada['aislante_conductor']
+        self.Taislante = self.datos_entrada[]
+        self.Tterminales = self.datos_entrada[]
+        self.material_conductor = self.datos_entrada[]
+        self.numero_conductores_por_fase = self.datos_entrada[]
+        self.misma_canalizacion = self.datos_entrada[]
+        self.ajuste_factor_carga = self.datos_entrada[]
+        self.porcentaje_Irating = self.datos_entrada[]
+        self.Interruptor_forzado = self.datos_entrada[]
+        self.canalizacion = self.datos_entrada[]
+        self.material_canalizacion = self.datos_entrada[]
+        self.tipo_conduit = self.datos_entrada[]
+        self.neutro_presente = self.datos_entrada[]
+            }
